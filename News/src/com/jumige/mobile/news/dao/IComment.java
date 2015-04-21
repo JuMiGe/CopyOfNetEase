ef542ec5a7fa19cc7456550953d5ff4626533ba9
Cyning Li <cyningli@shijiebang.net>
2015-04-21 23:10:13 +0800

    reset the project

--- News/src/com/jumige/mobile/news/dao/IComment.java
+++
@@ -1,9 +0,0 @@
-﻿package com.jumige.mobile.news.dao;
-
-import com.jumige.mobile.news.model.Comment;
-
-public interface IComment {
-
-	//根据ID获取评论
-	public Comment findCommById(Integer Id);
-}
---
+++ app/src/main/java/me/cyning/news/ui/MainActivity.java
@@ -0,0 +1,109 @@
+package me.cyning.news.ui;
+
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+
+import java.lang.reflect.Method;
+
+import me.cyning.news.R;
+
+
+public class MainActivity extends ActionBarActivity {
+    protected Toolbar toolbar;
+    protected DrawerLayout mDrawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        toolbar.setLogo(R.mipmap.base_common_default_icon_big);//����logo
+        setSupportActionBar(toolbar);//����toolbar
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+
+        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.open, R.string.close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+
+            }
+        };
+        mDrawerToggle.syncState();
+        mDrawerLayout.setDrawerListener(mDrawerToggle);//���ü�����
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        //������Ǹ�������
+        if (id == R.id.action_zone) {
+            return toggleDrawerLayout();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected boolean toggleDrawerLayout(){
+        if (mDrawerLayout.isDrawerOpen(Gravity.START)) {
+            mDrawerLayout.closeDrawer(Gravity.START);
+        return true;
+        }
+        if (mDrawerLayout.isDrawerOpen(Gravity.END)) {
+            mDrawerLayout.closeDrawer(Gravity.END);
+    } else {
+            mDrawerLayout.openDrawer(Gravity.END);
+
+
+        }
+        return true;
+    }
+    @Override
+    public boolean onMenuOpened(int featureId, Menu menu) {
+        if (featureId == Window.FEATURE_ACTION_BAR && menu != null) {
+            if (menu.getClass().getSimpleName().equals("MenuBuilder")) {
+                try {
+                    Method m = menu.getClass().getDeclaredMethod(
+                            "setOptionalIconsVisible", Boolean.TYPE);
+                    m.setAccessible(true);
+                    m.invoke(menu, true);
+                } catch (NoSuchMethodException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        return super.onMenuOpened(featureId, menu);
+    }
+
+}
